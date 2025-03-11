c612252d62cd216f287c6a7aee8e5725bfe2697f
lhuguetb <lhuguetb@gmail.com>
2025-03-11 16:51:10 +0100

    actualizar

---
+++ assets/classDiagram-v2-MQ7JQ4JX-CIjkmcmB.js
@@ -0,0 +1,1 @@
+import{s,c as e,a as r,b as l}from"./chunk-T2TOU4HS-Cudolh9M.js";import{_ as t}from"./index-C00PLJDC.js";import"./chunk-5HRBRIJM-CTsZmADz.js";var _={parser:l,db:r,renderer:e,styles:s,init:t(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute,r.clear()},"init")};export{_ as diagram};
